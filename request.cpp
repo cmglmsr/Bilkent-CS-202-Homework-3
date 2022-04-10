3bca14dcddb3ac4a2cac5a4caa5c256c43341c8e
unknown <cemg@server.com>
2022-04-10 18:51:34 +0300

    second commit with request.cpp request.h

---
+++ request.cpp
@@ -0,0 +1,10 @@
+
+
+/*
+* Title: request.h
+* Author: Mustafa Cem Gülümser
+* ID: 22003430
+* Section: 2
+* Assignment: 3
+* Description: Request class header file. Requests have priorities as key values
+*/
---
+++ request.h
@@ -0,0 +1,13 @@
+
+
+/*
+* Title: Heap.h
+* Author: Mustafa Cem Gülümser
+* ID: 22003430
+* Section: 2
+* Assignment: 3
+* Description: Heap class header file
+*/
+class request {
+
+};
